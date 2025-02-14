0c0fe065c57ea3371f895a25aae94d592daedd32
Utilisateur Lambda <clement.chassem@gmail.com>
2025-02-14 09:38:13 +0100

    feat(ui): mise à jour des textures et correction de l'initialisation du cube
    - Mise à jour des textures de l'interface utilisateur pour un meilleur rendu
    - Correction dans `RubikRotation.cs` pour éviter la recherche du cube avant sa génération
    - Amélioration de la gestion de l'initialisation pour éviter les erreurs

--- Rubik'scape/Assets/Scripts/Rubiks API/FetchCanvas.cs
+++ Rubik'scape/Assets/Scripts/Rubiks API/FetchCanvas.cs
@@ -12,6 +12,7 @@ public class FetchCanvas : MonoBehaviour
     [Header("Références Externes")]
     public RubiksAPIManager apiManager;
     public GameObject curvedUnityCanvas;
+    public GameObject rubikGenerator;
     public RubikGen rubikGen;
 
     public void Initialize(GameObject canvas, RubikGen gen) {
@@ -94,7 +95,7 @@ public class FetchCanvas : MonoBehaviour
                 Debug.Log($"Clé '{faceKey}' non trouvée dans faces_data.");
             }
         }
-
+        rubikGenerator.SetActive(true);
         // Initialise le Rubik's cube
         rubikGen.InitializeRubiksCube(level.name, level.cube_size, facesData);
     }
